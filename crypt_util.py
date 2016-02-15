b9b369debf0da07dfd6166690a5dbc25f3866716
Keith Bartolotta <ktbartolotta@gmail.com>
2016-02-15 16:30:16 -0500

    initial commit

---
+++ crypt_util.py
@@ -0,0 +1,171 @@
+import os
+import hashlib
+import argparse
+import getpass
+
+from Crypto.Cipher import AES
+from Crypto import Random
+
+
+def pad(data_str):
+
+    return data_str + (16 - len(data_str) % 16) * chr(16 - len(data_str) % 16)
+
+
+def unpad(data_str):
+
+    return data_str[0:-ord(data_str[-1])]
+
+
+def decrypt_message(msg, pwd):
+
+    try:
+        iv = msg[16:32]
+        salt = msg[:16]
+        dk = hashlib.pbkdf2_hmac('sha1', pwd, salt, 65536, 16)
+        enc_obj = AES.new(dk, AES.MODE_CBC, iv)
+        return unpad(enc_obj.decrypt(msg[32:]))
+    except Exception, e:
+        print(e)
+
+
+def decrypt_file(in_file_path, out_file_path, pwd):
+
+    try:
+        with open(in_file_path, 'rb') as input:
+            with open(out_file_path, 'wb') as output:
+                salt = input.read(16)
+                iv = input.read(16)
+                key = hashlib.pbkdf2_hmac('sha1', pwd, salt, 65536, 16)
+
+                block1 = input.read(16)
+                block2 = input.read(16)
+
+                dec_obj = AES.new(key, AES.MODE_CBC, iv)
+                content = dec_obj.decrypt(block1)
+                if block2 == '':
+                    output.write(unpad(content))
+                else:
+                    output.write(content)
+                    while True:
+
+                        iv = block1
+                        block1 = block2
+                        block2 = input.read(16)
+                        dec_obj = AES.new(key, AES.MODE_CBC, iv)
+                        content = dec_obj.decrypt(block1)
+                        if block2 != '':
+                            output.write(content)
+                        else:
+                            output.write(unpad(content))
+                            break
+    except Exception, e:
+        print(in_file_path, e)
+
+
+def decrypt_directory(in_dir_path, out_dir_path, pwd):
+
+    try:
+        if not os.path.isdir(out_dir_path):
+            os.mkdir(out_dir_path)
+        for root, dirs, files in os.walk(in_dir_path):
+            dir_path = os.sep.join(
+                [out_dir_path, root.replace(in_dir_path, '')])
+            if not os.path.isdir(dir_path):
+                os.mkdir(dir_path)
+            for f in files:
+                decrypt_file(
+                    os.path.join(root, f),
+                    os.path.join(dir_path, f)[:-4],
+                    pwd)
+    except Exception, e:
+        raise e
+
+
+def encrypt_message(msg, pwd):
+
+    try:
+        iv = Random.new().read(AES.block_size)
+        salt = Random.new().read(AES.block_size)
+        dk = hashlib.pbkdf2_hmac('sha1', pwd, salt, 65536, 16)
+        enc_obj = AES.new(dk, AES.MODE_CBC, iv)
+        return salt + iv + enc_obj.encrypt(pad(msg))
+    except Exception, e:
+        print(e)
+
+
+def encrypt_file(in_file_path, out_file_path, pwd):
+
+    try:
+        with open(in_file_path, 'rb') as input:
+            with open(out_file_path, 'wb') as output:
+                output.write(
+                    encrypt_message(input.read(), pwd))
+    except Exception, e:
+        print(in_file_path, e)
+
+
+def encrypt_directory(in_dir_path, out_dir_path, pwd):
+
+    try:
+        if not os.path.isdir(out_dir_path):
+            os.mkdir(out_dir_path)
+        for root, dirs, files in os.walk(in_dir_path):
+            dir_path = os.sep.join(
+                [out_dir_path, root.replace(in_dir_path, '')])
+            if not os.path.isdir(dir_path):
+                os.mkdir(dir_path)
+            for f in files:
+                encrypt_file(
+                    os.path.join(root, f),
+                    '.'.join([os.path.join(dir_path, f), 'enc']),
+                    pwd)
+    except Exception, e:
+        raise e
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        '-d', '--dir',
+        help='''Recursively decrypt all files in directory SOURCE and
+                place in directory TARGET.''',
+        nargs=2, metavar=('SOURCE', 'TARGET'))
+    group.add_argument(
+        '-f', '--file',
+        help='Decrypt file SOURCE and place in TARGET.', nargs=2,
+        metavar=('SOURCE', 'TARGET'))
+    group2 = parser.add_mutually_exclusive_group()
+    group2.add_argument(
+        '--encrypt',
+        help='Flag on for encrypt mode.',
+        action='store_true')
+    group2.add_argument(
+        '--decrypt',
+        help='Flag on for decrypt mode.',
+        action='store_true')
+    args = parser.parse_args()
+
+    password = getpass.getpass('Secret Key: ')
+
+    try:
+        if args.encrypt:
+            if args.file:
+                encrypt_file(args.file[0], args.file[1], password)
+            elif args.dir:
+                encrypt_directory(args.dir[0], args.dir[1], password)
+            else:
+                print('You for got to indicate file or directory.')
+        elif args.decrypt:
+            if args.file:
+                decrypt_file(args.file[0], args.file[1], password)
+            elif args.dir:
+                decrypt_directory(args.dir[0], args.dir[1], password)
+            else:
+                print('You forgot to indicate file or directory.')
+        else:
+            print('Decrypt or encrypt')
+    except Exception, e:
+        print(e)
---
+++ file_dialog_test.py
@@ -0,0 +1,213 @@
+import Tkinter
+import tkFileDialog
+import tkMessageBox
+import crypt_util
+import os
+
+
+class Application(Tkinter.Frame):
+
+    def __init__(self, root):
+
+        Tkinter.Frame.__init__(self, root)
+
+        self.source_path = Tkinter.StringVar()
+        self.target_path = Tkinter.StringVar()
+        self.crypt_mode = Tkinter.IntVar()
+        self.target_mode = Tkinter.IntVar()
+        self.password = Tkinter.StringVar()
+
+        crypt_group = Tkinter.LabelFrame(
+            self, text='Cryptographic Mode', padx=5, pady=5)
+        crypt_group.pack(padx=5, pady=5, fill=Tkinter.X)
+
+        Tkinter.Radiobutton(
+            crypt_group, text='Encrypt', variable=self.crypt_mode,
+            value=1).pack(side=Tkinter.LEFT)
+        Tkinter.Radiobutton(
+            crypt_group, text='Decrypt', variable=self.crypt_mode,
+            value=2).pack(side=Tkinter.LEFT)
+
+        target_group = Tkinter.LabelFrame(
+            self, text='Target a file or directory', padx=5, pady=5)
+        target_group.pack(padx=5, pady=5, fill=Tkinter.X)
+
+        Tkinter.Radiobutton(
+            target_group, text='File', variable=self.target_mode,
+            value=1, command=self.get_file_mode).pack(side=Tkinter.LEFT)
+        Tkinter.Radiobutton(
+            target_group, text='Directory', variable=self.target_mode,
+            value=2, command=self.get_dir_mode).pack(side=Tkinter.LEFT)
+
+        self.picker_container = Tkinter.Frame(self)
+        self.picker_container.pack(fill=Tkinter.X)
+        self.target_picker_group = Tkinter.Frame(self.picker_container)
+        self.target_picker_group.pack()
+
+        pwd_group = Tkinter.Frame(self, padx=5, pady=5)
+        pwd_group.pack(fill=Tkinter.X)
+        Tkinter.Label(pwd_group, text='Password:').pack(
+            padx=5, pady=5, side=Tkinter.LEFT)
+        Tkinter.Entry(pwd_group, textvariable=self.password).pack(
+            padx=5, pady=5, fill=Tkinter.X, side=Tkinter.LEFT)
+
+        self.control_group = Tkinter.Frame(self)
+        self.control_group.pack()
+        Tkinter.Button(
+            self.control_group, text='OK', command=self.process_crypt).pack(
+                padx=10, pady=5, side=Tkinter.LEFT)
+        Tkinter.Button(
+            self.control_group, text='Quit', command=self.quit).pack(
+                padx=10, pady=5, side=Tkinter.LEFT)
+
+        self.file_options = {
+            'filetypes': [('all files', '.*')],
+            'initialdir': '..\\',
+            'parent': root,
+            'title': 'The title'
+        }
+
+        self.dir_options = {
+            'initialdir': '..\\',
+            'parent': root,
+            'title': 'The title'
+        }
+
+    def get_file_mode(self):
+
+        self.target_picker_group.destroy()
+        self.target_path.set('')
+        self.source_path.set('')
+        self.target_picker_group = Tkinter.LabelFrame(
+            self.picker_container, text='Pick file')
+        self.target_picker_group.pack(padx=5, pady=5, fill=Tkinter.X)
+        source_group = Tkinter.Frame(self.target_picker_group)
+        source_group.pack(fill=Tkinter.X)
+        Tkinter.Button(
+            source_group, text='source',
+            command=self.ask_source_filename).pack(
+                side=Tkinter.LEFT, padx=5, pady=5)
+        Tkinter.Label(
+            source_group, text='stuff stuff stuff',
+            textvariable=self.source_path, bg='white').pack(
+                side=Tkinter.LEFT, fill=Tkinter.X, padx=5, pady=5)
+        target_group = Tkinter.Frame(self.target_picker_group)
+        target_group.pack(fill=Tkinter.X)
+        Tkinter.Button(
+            target_group, text='target',
+            command=self.ask_target_directory).pack(
+                side=Tkinter.LEFT, padx=5, pady=5)
+        Tkinter.Label(
+            target_group, text='stuff stuff stuff',
+            textvariable=self.target_path, bg='white').pack(
+                side=Tkinter.LEFT, fill=Tkinter.X, padx=5, pady=5)
+
+    def get_dir_mode(self):
+
+        self.target_picker_group.destroy()
+        self.target_path.set('')
+        self.source_path.set('')
+        self.target_picker_group = Tkinter.LabelFrame(
+            self.picker_container, text='Pick directory')
+        self.target_picker_group.pack(padx=5, pady=5, fill=Tkinter.X)
+        source_group = Tkinter.Frame(self.target_picker_group)
+        source_group.pack(fill=Tkinter.X)
+        Tkinter.Button(
+            source_group, text='source',
+            command=self.ask_source_directory).pack(
+                side=Tkinter.LEFT, padx=5, pady=5)
+        Tkinter.Label(
+            source_group, text='stuff stuff stuff',
+            textvariable=self.source_path, bg='white').pack(
+                side=Tkinter.LEFT, fill=Tkinter.X, padx=5, pady=5)
+        target_group = Tkinter.Frame(self.target_picker_group)
+        target_group.pack(fill=Tkinter.X)
+        Tkinter.Button(
+            target_group, text='target',
+            command=self.ask_target_directory).pack(
+                side=Tkinter.LEFT, padx=5, pady=5)
+        Tkinter.Label(
+            target_group, text='stuff stuff stuff',
+            textvariable=self.target_path, bg='white').pack(
+                side=Tkinter.LEFT, fill=Tkinter.X, padx=5, pady=5)
+
+    def ask_target_directory(self):
+
+        self.target_path.set(tkFileDialog.askdirectory(**self.dir_options))
+
+    def ask_source_directory(self):
+
+        self.source_path.set(tkFileDialog.askdirectory(**self.dir_options))
+
+    def ask_target_filename(self):
+
+        self.target_path.set(tkFileDialog.askopenfilename(**self.file_options))
+
+    def ask_source_filename(self):
+
+        self.source_path.set(tkFileDialog.askopenfilename(**self.file_options))
+
+    def quit(self):
+
+        self.master.destroy()
+
+    def process_crypt(self):
+        if self.password.get().strip() == '':
+            tkMessageBox.showwarning(
+                    'Password?', 'You need to specify a Password.')
+            return
+        if (os.path.isdir(self.source_path.get()) or (
+                    os.path.isfile(self.source_path.get()) and (
+                        os.path.isdir(self.target_path.get())))):
+
+            if self.crypt_mode.get() == 1:
+
+                if self.target_mode.get() == 1:
+                    target_file = '.'.join(
+                        [os.path.split(self.source_path.get())[1], 'enc'])
+                    target_path = os.path.join(
+                        self.target_path.get(), target_file)
+                    crypt_util.encrypt_file(
+                        self.source_path.get(),
+                        target_path, self.password.get())
+                elif self.target_mode.get() == 2:
+                    crypt_util.encrypt_directory(
+                        self.source_path.get(),
+                        self.target_path.get(),
+                        self.password.get())
+
+                tkMessageBox.showwarning(
+                    'Success!', 'Encryption complete.')
+
+            elif self.crypt_mode.get() == 2:
+
+                if self.target_mode.get() == 1:
+                    target_file = os.path.split(self.source_path.get())[1][:-4]
+                    target_path = os.path.join(
+                        self.target_path.get(), target_file)
+                    crypt_util.decrypt_file(
+                        self.source_path.get(),
+                        target_path, self.password.get())
+                elif self.target_mode.get() == 2:
+                    crypt_util.decrypt_directory(
+                        self.source_path.get(),
+                        self.target_path.get(),
+                        self.password.get())
+
+                tkMessageBox.showwarning(
+                    'Success!', 'Decryption complete.')
+            else:
+
+                tkMessageBox.showwarning(
+                    'Crypto?', 'You need to specify "Encrypt" or "Decrypt".')
+        else:
+            tkMessageBox.showwarning(
+                'Files?', '''You need to specify source file or directory
+                                  and a target directory.''')
+
+
+if __name__ == '__main__':
+
+    root = Tkinter.Tk()
+    Application(root).pack()
+    root.mainloop()
